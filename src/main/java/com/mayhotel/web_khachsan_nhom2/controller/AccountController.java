974273ae772dce43585ec1dcf9da2f9354e847e2
Tcuong2708 <vocuong1272@gmail.com>
2026-05-01 12:03:36 +0700

    Thêm phần đăng nhập, dùng cho phần đăng ký và quên mật khẩu

--- src/main/java/com/mayhotel/web_khachsan_nhom2/controller/AccountController.java
+++ src/main/java/com/mayhotel/web_khachsan_nhom2/controller/AccountController.java
@@ -1,4 +1,50 @@
 package com.mayhotel.web_khachsan_nhom2.controller;
 
-public class AccountController {
+import com.mayhotel.web_khachsan_nhom2.model.User;
+import com.mayhotel.web_khachsan_nhom2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class AccountController extends BaseController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/account/login")
+    public String login(Model model) {
+        setPageTitle(model, "Đăng nhập hệ thống");
+        setExtraCSS(model, "view/Account/login :: extra_css");
+        return render(model, "view/Account/login");
+    }
+
+    @PostMapping("/account/login")
+    public String postLogin(
+            @RequestParam("TenDangNhap") String username,
+            @RequestParam("MatKhau") String password,
+            HttpSession session,
+            Model model) {
+
+        User user = userRepository.findByTenDangNhap(username);
+
+        if (user != null && user.getMatKhau().equals(password)) {
+            session.setAttribute("user", user);
+            session.setAttribute("cartCount", 0);
+            return "redirect:/";
+        } else {
+            model.addAttribute("error", "Tên đăng nhập hoặc mật khẩu không chính xác!");
+            setPageTitle(model, "Đăng nhập hệ thống");
+            return render(model, "view/Account/login");
+        }
+    }
+
+    @GetMapping("/account/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:/Account/login";
+    }
 }
