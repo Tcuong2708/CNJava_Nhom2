d697e92d161706980066bcdb9d8b1119dbff267a
Tcuong2708 <vocuong1272@gmail.com>
2026-05-20 21:36:21 +0700

    Hoàn thiện chức năng OTP

--- src/main/java/com/mayhotel/web_khachsan_nhom2/model/User.java
+++ src/main/java/com/mayhotel/web_khachsan_nhom2/model/User.java
@@ -1,33 +1,34 @@
 package com.mayhotel.web_khachsan_nhom2.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Account")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    public Integer getIdTaiKhoan() {
-        return idTaiKhoan;
-    }
-
-    public void setIdTaiKhoan(Integer idTaiKhoan) {
-        this.idTaiKhoan = idTaiKhoan;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "IDTaiKhoan")
     private Integer idTaiKhoan;
 
-    @Column(name = "TenDangNhap")
+    @Column(name = "TenDangNhap", nullable = false)
     private String tenDangNhap;
 
-    @Column(name = "MatKhau")
+    @Column(name = "MatKhau", nullable = false)
     private String matKhau;
 
     @Column(name = "HoTen", columnDefinition = "NVARCHAR(255)")
     private String hoTen;
+
+    // BẮT BUỘC PHẢI CÓ ĐỂ CHẠY OTP
+    @Column(name = "Email", unique = true)
+    private String email;
 
     @Column(name = "SoDienThoai")
     private String soDienThoai;
@@ -35,66 +36,12 @@ public class User {
     @Column(name = "DiaChi", columnDefinition = "NVARCHAR(500)")
     private String diaChi;
 
-    @Column(name = "RoleID")
-    private Integer roleID;
-
-    public String getMatKhau() {
-        return matKhau;
-    }
-
-    public void setMatKhau(String matKhau) {
-        this.matKhau = matKhau;
-    }
-
-    public String getHoTen() {
-        return hoTen;
-    }
-
-    public void setHoTen(String hoTen) {
-        this.hoTen = hoTen;
-    }
-
-    public String getSoDienThoai() {
-        return soDienThoai;
-    }
-
-    public void setSoDienThoai(String soDienThoai) {
-        this.soDienThoai = soDienThoai;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
-
-    public Integer getRoleID() {
-        return roleID;
-    }
-
-    public void setRoleID(Integer roleID) {
-        this.roleID = roleID;
-    }
-
-    public String getQuocTich() {
-        return quocTich;
-    }
-
-    public void setQuocTich(String quocTich) {
-        this.quocTich = quocTich;
-    }
-
-    public String getTenDangNhap() {
-        return tenDangNhap;
-    }
-
-    public void setTenDangNhap(String tenDangNhap) {
-        this.tenDangNhap = tenDangNhap;
-    }
-
     @Column(name = "QuocTich", columnDefinition = "NVARCHAR(100)")
     private String quocTich;
 
+    @Column(name = "RoleID")
+    private Integer roleID;
+
+    @Column(name = "TrangThai")
+    private Boolean trangThai = true;
 }
