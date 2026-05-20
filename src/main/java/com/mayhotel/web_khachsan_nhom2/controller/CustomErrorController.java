f9bb7bc4b971c87397d2eeac695f1bf0e14e3416
Tcuong2708 <vocuong1272@gmail.com>
2026-05-20 21:53:02 +0700

    Trả về login

---
+++ src/main/java/com/mayhotel/web_khachsan_nhom2/controller/CustomErrorController.java
@@ -0,0 +1,14 @@
+package com.mayhotel.web_khachsan_nhom2.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "redirect:/account/login";
+    }
+}
